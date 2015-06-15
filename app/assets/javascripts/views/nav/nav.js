4c2206567dfa1e4ec45c03be786ad06185cd5605
Eric Ashmore <edashmore@gmail.com>
2015-06-14 21:46:33 -0700

    user search bar

--- app/assets/javascripts/views/nav/nav.js
+++ app/assets/javascripts/views/nav/nav.js
@@ -4,17 +4,32 @@ GoodgamesApp.Views.Nav = Backbone.View.extend({
 
   events: {
     'click #profile': 'toProfile',
-    'click #games-link': 'toGamesIndex'
+    'click #games-link': 'toGamesIndex',
+    'click .search-games': 'searchGamesBar',
+    'click .search-users': 'searchUserBar'
   },
 
-  initialize: function (options) {},
+  initialize: function () {
+
+  },
 
   render: function () {
     this.$el.html(this.template);
-    var searchView = new GoodgamesApp.Views.SearchBar();
-    this.$el.find('#search-bar').html(searchView.render().$el);
+    this.searchGamesBar();
 
     return this;
+  },
+
+  searchGamesBar: function (event) {
+    event && event.preventDefault();
+    var searchView = new GoodgamesApp.Views.SearchBar();
+    this._swapSearch(searchView);
+  },
+
+  searchUserBar: function(event) {
+    event.preventDefault();
+    var searchView = new GoodgamesApp.Views.UserSearchBar();
+    this._swapSearch(searchView);
   },
 
   toProfile: function (event) {
@@ -29,5 +44,11 @@ GoodgamesApp.Views.Nav = Backbone.View.extend({
     $(".nav").find(".active").removeClass("active");
     $('.games-link').addClass("active");
     Backbone.history.navigate('', { trigger: true });
+  },
+
+  _swapSearch: function (searchView) {
+    this._currentSearch && this._currentSearch.remove();
+    this._currentSearch = searchView;
+    this.$el.find('#search-bar').html(searchView.render().$el);
   }
 });
