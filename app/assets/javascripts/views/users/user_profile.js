039792dbe2927799e6c00cb62540de252c29babd
Eric Ashmore <edashmore@gmail.com>
2015-06-16 15:02:46 -0700

    add slick

--- app/assets/javascripts/views/users/user_profile.js
+++ app/assets/javascripts/views/users/user_profile.js
@@ -71,7 +71,43 @@ GoodgamesApp.Views.UserProfile = Backbone.CompositeView.extend({
 
     this.attachSubviews();
 
-    return this;
+    this.slickSlider();
+  },
+
+  slickSlider: function () {
+    $('.slider').slick({
+      dots: true,
+      infinite: false,
+      speed: 300,
+      slidesToShow: 5,
+      slidesToScroll: 5,
+      variableWidth: true,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 5,
+            slidesToScroll: 5,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 4,
+            slidesToScroll: 4
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3
+          }
+        }
+      ]
+    });
   },
 
   upload: function(event){
