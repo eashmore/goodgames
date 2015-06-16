62577a40cacd03a94beea6ee90391c65de36197c
Eric Ashmore <edashmore@gmail.com>
2015-06-16 11:46:37 -0700

    fix for heroku assests

--- config/application.rb
+++ config/application.rb
@@ -22,7 +22,7 @@ module GoodgamesApp
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
-    config.serve_static_files = true
+    config.serve_static_assets = true
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
