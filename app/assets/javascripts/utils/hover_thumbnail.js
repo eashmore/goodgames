fcc9d14464f97f666f99c86a5486d87945395c10
Eric Ashmore <edashmore@gmail.com>
2015-06-17 16:57:26 -0700

    more css

--- app/assets/javascripts/utils/hover_thumbnail.js
+++ app/assets/javascripts/utils/hover_thumbnail.js
@@ -11,14 +11,11 @@
 // });
 
 $(function () {
-  setTimeout( function () {
-    $("#main").on("mouseenter", '.game-thumb', (function () {
-      $(this).find(".index-boxart").css('opacity', 0.3);
-      $(this).find('.rate').css('z-index', 10);
-    })).on('mouseleave', '.game-thumb', (function () {
-      $(this).find(".index-boxart").css('opacity', 1);
-      $(this).find('.rate').css('z-index', -10);
-
-    }));
-  }, 500);
+  $("#main").on("mouseenter", '.game-thumb', (function () {
+    $(this).find(".index-boxart").css('opacity', 0.3).css('box-shadow', '0px 0px 15px #fff');
+    $(this).find('.rate').css('z-index', 10);
+  })).on('mouseleave', '.game-thumb', (function () {
+    $(this).find(".index-boxart").css('opacity', 1).css('box-shadow', '0px 0px 5px #98f5ff');
+    $(this).find('.rate').css('z-index', -10);
+  }));
 });
