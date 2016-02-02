d2cafa722891a79711261ea1a0a7e7ea14230968
Eric Ashmore <edashmore@gmail.com>
2016-02-01 21:50:35 -0800

    refactor(games): finding highest scoring games is now own function

--- app/assets/javascripts/views/games/games_index.js
+++ app/assets/javascripts/views/games/games_index.js
@@ -4,45 +4,38 @@ GoodgamesApp.Views.GamesIndex = Backbone.CompositeView.extend({
 
   className: 'container-fluid',
 
-  initialize: function (options) {
+  initialize: function(options) {
     $('.nav-games').addClass("active");
 
     this.currentUser = options.currentUser;
-    this.highestRated = [26839, 22420, 1539, 15473, 2600];
     this.listenTo(this.collection, 'sync', this.render);
     this.addFeed();
   },
 
-  render: function () {
+  render: function() {
     var content = this.template();
     this.$el.html(content);
-    this.addGames();
+    if (this.collection.length) {
+      this.addGames();
+    }
     this.attachSubviews();
 
     return this;
   },
 
-  addGames: function () {
+  addGames: function() {
     var popular = [36765, 36113, 46569, 35573, 24024];
     var newReleases = [41484, 45577, 46582, 47342, 49073];
     var classics = [12572, 10299, 18115, 8307, 8870];
-    if (GoodgamesApp.games.length) {
-      var sortedGames = GoodgamesApp.games.sortBy(function (game) {
-        return -game.escape('score');
-      });
-      this.highestRated = [];
-      sortedGames.slice(0, 5).forEach(function (game) {
-        this.highestRated.push(game.id);
-      }.bind(this));
-    }
+    var highestRated = this.getHighestRated();
 
     this.addGame(popular, '#popular');
     this.addGame(newReleases, '#new');
-    this.addGame(this.highestRated, '#best');
+    this.addGame(highestRated, '#best');
     this.addGame(classics, '#old');
   },
 
-  addGame: function (array, index) {
+  addGame: function(array, index) {
     array.forEach(function (game_id) {
       var game = GoodgamesApp.games.where({ id: game_id })[0];
       var gameView = new GoodgamesApp.Views.GameItem({ model: game });
@@ -50,7 +43,20 @@ GoodgamesApp.Views.GamesIndex = Backbone.CompositeView.extend({
     }.bind(this));
   },
 
-  addFeed: function () {
+  getHighestRated: function() {
+    var highestRated = [];
+    var sortedGames = GoodgamesApp.games.sortBy(function (game) {
+      return -game.escape('score');
+    });
+
+    sortedGames.slice(0, 5).forEach(function(game) {
+      highestRated.push(game.id);
+    }.bind(this));
+
+    return highestRated;
+  },
+
+  addFeed: function() {
     var recFeedView = new GoodgamesApp.Views.CollectionFeed();
     this.addSubview('#recommendation-feed', recFeedView);
   }
